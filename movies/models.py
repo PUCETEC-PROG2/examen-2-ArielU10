6862b38476bd85738b334f9c3a8500476cb10476
Brandom Ortega <baortega@puce.edu.ec>
2024-07-13 12:10:59 -0500

    Evaluacion Parcial 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    id = models.IntegerField(null=False)
+    MOVIE_GENRE = {
+        ("T", "Terror"),
+        ("A", "Accion"),
+        ("C", "Comedia"),
+        ("D", "Drama"),
+    }
+    title = models.CharField(max_length=30, null=False)
+    genre = models.CharField(max_length=30, choices=MOVIE_GENRE, null=False)
+    director_name = models.CharField(max_length=30, null=False)
+    year = models.IntegerField(null=False)
+    synopsis = models.TextField
+    
+    def __str__(self) -> str:
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
 # Create your views here.
+
+def index(request):
+    movies = Movie.objects.order_by('genre') ## SELECT * FROM 
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movie(request, movie_id):
+    #SELECT * FROM movie_movie WHERE id='movie_id'
+    pokemon = Movie.objects.get(id=movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie 
+    }
+    return HttpResponse(template.render(context, request))
